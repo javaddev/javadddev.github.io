c7fd8ad0c65bb02adbdde3105fd8bdd2758d3aaa
javadddev <javadddev@users.noreply.github.com>
2022-08-02 10:36:42 +0000

    deploy: 99b69c1324ad78f9688c3014553913f58a364ddd

--- _nuxt/static/1659436292/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fjavadddev.github.io\u002F_nuxt\u002Fstatic\u002F1659436292",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fjavadddev.github.io\u002F",assetsPath:"\u002Fjavadddev.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"735606ef"}}}}(null));
